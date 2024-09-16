b316d7464ba023859916ebbc2dfa3a2028154e3b
Siddhesh.More <siddhesh.more@wdimails.com>
2024-09-16 20:41:36 +0530

    uddatee

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     <Router>
       <Routes>
   
-      <Route path="/" element={<Home />} />
+      <Route path="/cards/" element={<Home />} />
       <Route path="/cards" element={<Cards />} />
       
       </Routes>
