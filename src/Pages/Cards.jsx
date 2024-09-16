cfc1ada47db5af51b7b1217e3c4c15fa45ef30f7
Siddhesh.More <siddhesh.more@wdimails.com>
2024-09-16 22:49:15 +0530

    upddate

--- src/Pages/Cards.jsx
+++ src/Pages/Cards.jsx
@@ -1,61 +1,140 @@
-import { Box, Button, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Image, Text, useDisclosure } from "@chakra-ui/react";
 import React, { useState } from "react";
-import floral from '../assets/floral.png'
+import floral from "../assets/floral.png";
 import { GiImperialCrown, GiQueenCrown } from "react-icons/gi";
+import LevelCompleteModal from "./LevelCompleteModal";
 
 const Cards = () => {
-    const cardsLevelOne = [
-        {
-          id: 1,
-          task: "Complete the monthly financial report",
-          elseTask: "Review last month's budget allocations",
-        },
-        {
-          id: 2,
-          task: "Prepare presentation for client meeting",
-          elseTask: "Email the final draft to the marketing team",
-        },
-        {
-          id: 3,
-          task: "Fix UI bug on the user profile page",
-          elseTask: "Cross-test on different browsers for compatibility",
-        },
-        {
-          id: 4,
-          task: "Respond to customer feedback on support tickets",
-          elseTask: "Follow up on unresolved high-priority issues",
-        },
-        {
-          id: 5,
-          task: "Organize team building event for next month",
-          elseTask: "Book a venue and send out invitations",
-        },
-        {
-          id: 6,
-          task: "Update product documentation with new features",
-          elseTask: "Review technical changes with the dev team",
-        },
-        {
-          id: 7,
-          task: "Analyze social media engagement metrics",
-          elseTask: "Draft report on insights and growth strategies",
-        },
-        {
-          id: 8,
-          task: "Backup database and archive old data",
-          elseTask: "Verify that backups were successfully completed",
-        },
-        {
-          id: 9,
-          task: "Schedule a one-on-one with team members",
-          elseTask: "Discuss project progress and individual goals",
-        },
-        {
-          id: 10,
-          task: "Research industry trends for quarterly newsletter",
-          elseTask: "Draft the outline for the next publication",
-        },
-      ];
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  const cardsLevelOne = [
+    {
+      id: 1,
+
+      level: 1,
+      task: "Complete the monthly financial report",
+      elseTask: "Review last month's budget allocations",
+    },
+    {
+      id: 2,
+      level: 1,
+      task: "Prepare presentation for client meeting",
+      elseTask: "Email the final draft to the marketing team",
+    },
+    {
+      id: 3,
+      level: 1,
+      task: "Fix UI bug on the user profile page",
+      elseTask: "Cross-test on different browsers for compatibility",
+    },
+    {
+      id: 4,
+      level: 1,
+      task: "Respond to customer feedback on support tickets",
+      elseTask: "Follow up on unresolved high-priority issues",
+    },
+    {
+      id: 5,
+      level: 1,
+      task: "Organize team building event for next month",
+      elseTask: "Book a venue and send out invitations",
+    },
+    {
+      id: 6,
+      level: 1,
+      task: "Update product documentation with new features",
+      elseTask: "Review technical changes with the dev team",
+    },
+    {
+      id: 7,
+      level: 1,
+      task: "Analyze social media engagement metrics",
+      elseTask: "Draft report on insights and growth strategies",
+    },
+    {
+      id: 8,
+      level: 1,
+      task: "Backup database and archive old data",
+      elseTask: "Verify that backups were successfully completed",
+    },
+    {
+      id: 9,
+      level: 1,
+      task: "Schedule a one-on-one with team members",
+      elseTask: "Discuss project progress and individual goals",
+    },
+    {
+      id: 10,
+      level: 1,
+      task: "Research industry trends for quarterly newsletter",
+      elseTask: "Draft the outline for the next publication",
+    },
+  ];
+
+
+  const cardsLevelTwo = [
+    {
+      id: 1,
+
+      level: 2,
+      task: "Complete the monthly financial report",
+      elseTask: "Review last month's budget allocations",
+    },
+    {
+      id: 2,
+      level: 2,
+      task: "Prepare presentation for client meeting",
+      elseTask: "Email the final draft to the marketing team",
+    },
+    {
+      id: 3,
+      level: 2,
+      task: "Fix UI bug on the user profile page",
+      elseTask: "Cross-test on different browsers for compatibility",
+    },
+    {
+      id: 4,
+      level: 2,
+      task: "Respond to customer feedback on support tickets",
+      elseTask: "Follow up on unresolved high-priority issues",
+    },
+    {
+      id: 5,
+      level: 2,
+      task: "Organize team building event for next month",
+      elseTask: "Book a venue and send out invitations",
+    },
+    {
+      id: 6,
+      level: 2,
+      task: "Update product documentation with new features",
+      elseTask: "Review technical changes with the dev team",
+    },
+    {
+      id: 7,
+      level: 2,
+      task: "Analyze social media engagement metrics",
+      elseTask: "Draft report on insights and growth strategies",
+    },
+    {
+      id: 8,
+      level: 2,
+      task: "Backup database and archive old data",
+      elseTask: "Verify that backups were successfully completed",
+    },
+    {
+      id: 9,
+      level: 2,
+      task: "Schedule a one-on-one with team members",
+      elseTask: "Discuss project progress and individual goals",
+    },
+    {
+      id: 10,
+      level: 2,
+      task: "Research industry trends for quarterly newsletter",
+      elseTask: "Draft the outline for the next publication",
+    },
+  ];
+
 
   const [availableTasks, setAvailableTasks] = useState([...cardsLevelOne]);
   const [selectedTask, setSelectedTask] = useState(null);
@@ -63,8 +142,9 @@ const Cards = () => {
 
   const getRandomTask = (pusher) => {
     if (availableTasks.length === 0) {
-    //   alert('No more tasks available!');
-      setAvailableTasks([...cardsLevelOne])
+      //   alert('No more tasks available!');
+    //   setAvailableTasks([...cardsLevelOne]);
+    onOpen()
       return;
     }
 
@@ -73,21 +153,30 @@ const Cards = () => {
     const task = availableTasks[randomIndex];
 
     // Update rotation based on pusher (king or queen)
-    const rotationAngle = pusher === "king" ? -182 : 182; // Rotate up for king, down for queen
+    const rotationAngle = pusher === "king" ? -180 : 180; // Rotate up for king, down for queen
     setRotation(rotation + rotationAngle); // Accumulate rotation
 
     // Remove the selected task from the available tasks list
-    const newAvailableTasks = availableTasks.filter((_, index) => index !== randomIndex);
-    
+    const newAvailableTasks = availableTasks.filter(
+      (_, index) => index !== randomIndex
+    );
+
     // Update state
     setSelectedTask(task);
     setAvailableTasks(newAvailableTasks);
   };
 
+  const handleOnClose=()=>{
+    setAvailableTasks(cardsLevelTwo)
+    onClose()
+  }
+
+  
+
   return (
     <Box
       overflow={"auto"}
-      bg={"#1e272e"}
+      bg={selectedTask?.level === 1 ? "#ecf0f1" :"#ecf0f1"}
       flexDirection={"column"}
       gap={8}
       py={4}
@@ -112,7 +201,6 @@ const Cards = () => {
         rounded={"full"}
         color={"#fff"}
         bg="#7c0000"
-
         id="btn"
       >
         <GiQueenCrown fontSize={47} />
@@ -127,8 +215,8 @@ const Cards = () => {
         display={"flex"}
         flexDirection={"column"}
         justifyContent={"start"}
-        bg={"#000"}
-        color={'#fff'}
+        bg={selectedTask?.level === 1 ? "#fff" :"#000"}
+        color={selectedTask?.level === 1 ? "#000":"#fff"}
         boxShadow={"lg"}
         rounded={"xl"}
         w={280}
@@ -164,7 +252,7 @@ const Cards = () => {
 
       {/* King Button */}
       <Box
-    //   as="button"
+        //   as="button"
         onClick={() => getRandomTask("king")}
         transition={"all 0.5s"}
         _focus={{ bg: "#000", outline: "none", border: "none" }}
@@ -181,6 +269,7 @@ const Cards = () => {
       >
         <GiImperialCrown fontSize={47} />
       </Box>
+      <LevelCompleteModal level={selectedTask?.level} onClose={handleOnClose} isOpen={isOpen} onOpen={onOpen} />
     </Box>
   );
 };
---
+++ src/Pages/LevelCompleteModal.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalBody,
+    Image,
+    Text,
+  } from '@chakra-ui/react'
+  import levelone from '../assets/level1.png'
+  import leveltwo from '../assets/level5.png'
+  import levelthree from '../assets/level3.png'
+  import levelfour from '../assets/level4.png'
+  import levelfive from '../assets/level5.png'
+
+  function LevelCompleteModal({ isOpen, onClose, level }) {
+    
+    // Determine background color based on the level
+    const getBackgroundColor = () => {
+      switch (level) {
+        case 1:
+          return '#ff6a00'; // Lighter red for level 1
+        case 2:
+          return '#000'; // Tomato color for level 2
+        case 3:
+          return '#7c0000'; // Red for level 3
+        case 4:
+          return '#3e0000'; // Darker red for level 4
+        case 5:
+          return '#000'; // Darkest red for level 5
+        default:
+          return '#000'; // Default dark color if level is undefined
+      }
+    };
+
+    // Select the image based on the level
+    const getImageForLevel = () => {
+      switch (level) {
+        case 1:
+          return levelone;
+        case 2:
+          return leveltwo;
+        case 3:
+          return levelthree;
+        case 4:
+          return levelfour;
+        case 5:
+          return levelfive;
+        default:
+          return levelone; // Fallback to level 1 image if level is undefined
+      }
+    };
+
+    // Add quotes specific to each level
+    const getQuoteForLevel = () => {
+      switch (level) {
+        case 1:
+          return "The journey begins. Brace yourself!";
+        case 2:
+          return "Keep up the momen!";
+        case 3:
+          return "Halfway there. No time to look back!";
+        case 4:
+          return "The end is near, but the challenge rises!";
+        case 5:
+          return "Victory is at hand. Go for glory!";
+        default:
+          return "Let the action begin"; // Default text
+      }
+    };
+
+    return (
+      <>
+        <Modal size={'full'} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent 
+              h="100vh" // Full height
+              w="100vw" // Full width
+              >
+            <ModalBody 
+              onDoubleClick={onClose} 
+              display={'flex'} 
+              gap={12} 
+              justifyContent={'center'} 
+              alignItems={'center'} 
+              flexDirection={'column'} 
+              bg={getBackgroundColor()} // Dynamic background color
+
+            >
+              <Image w={'80%'} src={getImageForLevel()} /> {/* Dynamic image */}
+              <Text fontSize={'3xl'} color={level === 1 ? "#fff" : "#000"}> {/* Text color changes for level 1 */}
+                {getQuoteForLevel()} {/* Dynamic quote */}
+              </Text>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
+
+export default LevelCompleteModal;
